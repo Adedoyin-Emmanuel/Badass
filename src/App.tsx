9fcb48318228ed04c7a22825b1f57ac441b77b5e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-03 14:19:45 +0100

    Added the page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return (
+        <React.Fragment>
+            <h2>This is the home component</h2>
+        </React.Fragment>
+    )
+}
+
+
+export default Home;
