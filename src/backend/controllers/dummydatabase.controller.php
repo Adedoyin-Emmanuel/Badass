083504bdb023088e112ccf90b02e83d1522a97b5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 06:29:57 +0100

    Updated the dummy database

--- src/backend/controllers/dummydatabase.controller.php
+++ src/backend/controllers/dummydatabase.controller.php
@@ -4,4 +4,38 @@ require_once "dbaseconnector.controller.php";
 
 use Controllers\Database\DatabaseConnector;
 
-$database = new DatabaseConnector();
+/**
+ * 
+ * Base class for the user controller class
+ * @author: Adedoyin Emmanuel Adeniyi
+ */
+
+
+final class UserController extends DatabaseConnector
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_users()
+    {
+        $get_users_query = "SELECT * FROM users WHERE status = ? OR ?";
+
+        if ($statement = $this->conn->prepare($get_users_query))
+         {
+            $statement->bind_param("ii", 1, 0);
+            $statement->execute();
+
+            $result = $statement->get_result();
+
+            if (!$result)
+            {
+                return "an error occured" . $this->conn->error;
+            }
+            $user_row = $result->fetch_assoc();
+
+            return $user_row;
+        }
+    }
+}
