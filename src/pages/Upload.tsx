62bb1ab8a75139427798ab2736fb140848888a4e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:19:01 +0100

    Updated the Upload page

---
+++ src/pages/Upload.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Upload = () =>{
+    return (
+        <React.Fragment>
+            <h3>Hello, comrade upload a legit file!</h3>
+        </React.Fragment>
+    )
+}
+
+
+export default Upload;
