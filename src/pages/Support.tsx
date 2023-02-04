aca5149a7c184ba2cbffaebe8ca9ce2a44730d7e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:12:55 +0100

    Updated the support page

---
+++ src/pages/Support.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Support = () =>{
+    return (
+        <React.Fragment>
+            <h3>Support Your Guy Abeg!</h3>
+        </React.Fragment>
+    )
+}
+
+
+export default Support;
