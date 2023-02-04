5286e1a6d6a6754224b4344aa744d07021eb0c80
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:13:16 +0100

    Imported the support page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Support from "./pages/Support";
 function App() {
 	return (
 		<div className="App">
