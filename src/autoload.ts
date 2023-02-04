ab1a0e37b80a16b80a6c47d0281d4bacf11b70b9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:58:06 +0100

    Updated the autoload file

--- src/autoload.ts
+++ src/autoload.ts
@@ -1,2 +1,10 @@
+//import the bootstrap library
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css.map";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.min.js.map";
+
+
+//custom styles and spinner animation file
 import "./styles/styles.css";
 import "./styles/three-dots.css";
