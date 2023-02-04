f7ec389165b6f5c565cdacc2b6d3d02fe64e2beb
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:20:09 +0100

    Updated the search page

---
+++ src/pages/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () =>{
+    return (
+        <React.Fragment>
+            <h3>Search anything!</h3>
+        </React.Fragment>
+    )
+}
+
+
+export default Search;
