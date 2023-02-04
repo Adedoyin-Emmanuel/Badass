ad7009a2391a62038fedd257524e2f31cbc1ae76
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:06:30 +0100

    updated the index.ts comments

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+//we go add the service worker later :)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
