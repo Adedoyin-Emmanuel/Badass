c215c23553620f9f593af0ca1505836ea7665da3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-04 21:20:34 +0100

    Updated the convert page

--- src/pages/Convert.tsx
+++ src/pages/Convert.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Home = () =>{
+const Convert = () =>{
     return (
         <React.Fragment>
             <h2>This is the home component</h2>
@@ -9,4 +9,4 @@ const Home = () =>{
 }
 
 
-export default Home;
+export default Convert;
