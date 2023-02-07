423be74490f70eb2fdb6d20267518f3d2247b8f7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-07 03:00:59 +0100

    Bug fix

--- src/apis/getData.ts
+++ src/apis/getData.ts
@@ -3,9 +3,11 @@ import jQuery from "jquery";
 const sayHello = () => {
 	jQuery(($) => {
 		$.ajax({
-			url: "https://badass-backend.up.railway.app/",
-			success: (result:any) => {
-				console.log(result);
+			url: "http://localhost/badass-backend/api",
+			success: (results:any) => {
+				const $result = JSON.parse(results);
+
+				console.log($result.code);
 			},
 			error: (error) => {
 				console.error(error);
