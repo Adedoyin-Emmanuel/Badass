d17b29d2db2a1b5d4aa3a0ae3df82d9918d2697f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-08 07:27:21 +0100

    Added the header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,70 @@
+const Header = () => {
+	return (
+		<nav
+			className="navbar navbar-expand-sm navbar-dark bg-dark"
+			aria-label="Third navbar example">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Expand at sm
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarsExample03"
+					aria-controls="navbarsExample03"
+					aria-expanded="true"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="navbar-collapse collapse show" id="navbarsExample03">
+					<ul className="navbar-nav me-auto mb-2 mb-sm-0">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Link
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link disabled">Disabled</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								data-bs-toggle="dropdown"
+								aria-expanded="false">
+								Dropdown
+							</a>
+							<ul className="dropdown-menu">
+								<li>
+									<a className="dropdown-item" href="#">
+										Action
+									</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="#">
+										Another action
+									</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="#">
+										Something else here
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
 
-
-export {}
+export default Header;
