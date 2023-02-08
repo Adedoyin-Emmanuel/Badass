e1e54db9a308db24f7f5c518f8eb0ad1fa780fc7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-08 07:20:39 +0100

    Added the spinner component

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,3 +1,8 @@
-
-
-export {}
+const Spinner = () => {
+	return (
+		<div id="spinner" className="spinner">
+			<div className="dot-windmill spinner-child" id="spinner-child"></div>
+		</div>
+	);
+};
+export default Spinner;
