36be5942757a9133625a0c8680a58c345061a4c0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-08 07:12:36 +0100

    Modified the assets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ const Home = () => {
 	return (
 		<React.Fragment>
 			<h2>This is the home component</h2>
+
+			<div className="bg-dark">
+			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="52" height="52"/>
+			</div>
 		</React.Fragment>
 	);
 };
