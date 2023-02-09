603201dadee3b4678eb8a1686a622c2fad194bb2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-09 17:33:16 +0100

    Updated the hero section

---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () =>{
+    return (
+        <React.Fragment>
+            <section className="hero-section">
+                
+            </section>
+        </React.Fragment>
+    )
+}
