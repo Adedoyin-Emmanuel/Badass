0ca3afad96b6c4884769e710f830089194ee69fe
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-09 17:34:45 +0100

    Added the Hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,9 +3,11 @@ import React from "react";
 const Hero = () =>{
     return (
         <React.Fragment>
-            <section className="hero-section">
-                
+            <section className="hero-section brand-dark-color">
+
             </section>
         </React.Fragment>
     )
 }
+
+export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import sayHello from "./../apis/getData";
 import Header from "../components/header";
 import MainPageFooter from "../components/main-page-footer";
-
+import Hero from "../components/hero";
 const Home = ()=> {
 
 	return (
