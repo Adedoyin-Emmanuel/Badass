8e317a91a280fd983f40fc3129261023d5c1a456
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-09 05:46:19 +0100

    Added the page footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import sayHello from "./../apis/getData";
 import Header from "../components/header";
+import MainPageFooter from "../components/main-page-footer";
 const Home = () => {
 
 	return (
 		<React.Fragment>
 			<Header/>
 
-			
+			<MainPageFooter/>
 		</React.Fragment>
 	);
 };
