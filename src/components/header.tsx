f98fef44d5d95b8300c08c8fd5ee8334c2754268
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-09 06:40:05 +0100

    Bug fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,83 +1,88 @@
+import React from "react";
+
 const Header = () => {
 	return (
-		<nav
-			className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top"
-			aria-label="Badass navbar" style={{"display":"fixed"}}>
-			<div className="container-fluid">
-				<a className="navbar-brand fw-bold" href="#">
-					Badass
-				</a>
-				<button
-					className="btn mx-0"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarsExample03"
-					aria-controls="navbarsExample03"
-					aria-expanded="false"
-					aria-label="Toggle navigation"	
-					>
-					<span className="">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width={"27px"}
-						height={"27px"}
-						viewBox="0 0 24 24"
-						className="d-block"
-						>
-						<path fill="none" className="bg-dark" d="M0 0h24v24H0V0z" />
-						<path
-							fill="white"
-							d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z"
-						/>
-					</svg>
-					</span>
-				</button>
+		<React.Fragment>
+			<nav
+				className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top"
+				aria-label="Badass navbar"
+				style={{ display: "fixed" }}>
+				<div className="container-fluid">
+					<a className="navbar-brand fw-bold" href="#">
+						Badass
+					</a>
+					<button
+						className="btn mx-0"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarsExample03"
+						aria-controls="navbarsExample03"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width={"27px"}
+								height={"27px"}
+								viewBox="0 0 24 24"
+								className="d-block">
+								<path fill="none" className="bg-dark" d="M0 0h24v24H0V0z" />
+								<path
+									fill="white"
+									d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z"
+								/>
+							</svg>
+						</span>
+					</button>
 
-				<div className="collapse navbar-collapse" id="navbarsExample03">
-					<ul className="navbar-nav me-auto mb-2 mb-sm-0">
-						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Link
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link disabled">Disabled</a>
-						</li>
-						<li className="nav-item dropdown">
-							<a
-								className="nav-link dropdown-toggle"
-								href="#"
-								data-bs-toggle="dropdown"
-								aria-expanded="false">
-								Dropdown
-							</a>
-							<ul className="dropdown-menu">
-								<li>
-									<a className="dropdown-item" href="#">
-										Action
-									</a>
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Another action
-									</a>
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Something else here
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
+					<div className="collapse navbar-collapse" id="navbarsExample03">
+						<ul className="navbar-nav me-auto mb-2 mb-sm-0">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Link
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link disabled">Disabled</a>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle"
+									href="#"
+									data-bs-toggle="dropdown"
+									aria-expanded="false">
+									Dropdown
+								</a>
+								<ul className="dropdown-menu">
+									<li>
+										<a className="dropdown-item" href="#">
+											Action
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Another action
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Something else here
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+
+			<br /><br/><br/>
+		</React.Fragment>
 	);
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import sayHello from "./../apis/getData";
 import Header from "../components/header";
 import MainPageFooter from "../components/main-page-footer";
+
 const Home = () => {
 
 	return (
 		<React.Fragment>
 			<Header/>
+			<br/>
+			<br/>
+			<br/>
+		<div className="brand-secondary-color">
 
+
+		<div className="box p-4" >
+		i am emmysoft
+		</div>
+
+		</div>
 			<MainPageFooter/>
 		</React.Fragment>
 	);
