39b701fbaf514166efb25ffd27f104b3f596f9da
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 12:39:13 +0100

    Added the database.ts file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import sayHello from "./../apis/getData";
 import Header from "../components/header";
-import MainPageFooter from "../components/main-page-footer";
+
 
 const Home = ()=> {
 
