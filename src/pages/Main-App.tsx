4707331c1d2851ed1a3828792129368be7dc9f86
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 16:08:51 +0100

    Added the redirect route

--- src/pages/Main-App.tsx
+++ src/pages/Main-App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import db from "../backend/db";
 const MainApp = (): JSX.Element =>{
     return (
         <React.Fragment>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import db from "./../backend/db";
+import { navigate } from '../includes/scripts/script';
 
 const Settings = (): JSX.Element =>{
+    if(!(db.get("BADASS_HOME_PAGE_SEEN") === "true"))
+    {
+        navigate("/home");
+    }
+    
     return (
         <React.Fragment>
             
