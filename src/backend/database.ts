4f58d03148902ac9309e5f53562cc776e1e8f238
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 14:48:56 +0100

    Added the Db.ts file and updated the database.ts file

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -1,1 +1,30 @@
-export {}
+/**
+ * Base Class For localstorage  DB custom library
+ **/
+
+class Database {
+	get(key: string): string | undefined {
+		let data = localStorage.getItem(key);
+
+		return data != null ? data : undefined;
+	}
+
+	create(key: string, value: string): void {
+		key == undefined || value == undefined
+			? console.warn("cannot create db item, key or value not specified")
+			: localStorage.setItem(key, value);
+	}
+
+	update(key: string, value: string): void {
+		this.create(key, value);
+	}
+	delete(key: string): void {
+		this.update(key, "NULL");
+	}
+	destroy(): void {
+		localStorage.clear();
+	}
+}
+
+
+export default Database;
