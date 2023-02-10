042d58d69e397db3122a91b1c7017d3f9d8b24a4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 15:39:51 +0100

    Imported the search component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Support from "./pages/Support";
 import MainApp from "./pages/Main-App";
+import Search from "./pages/Search";
 import "./autoload";
 import db from "./backend/db";
 function App() {
-
+  
+  //default route is the main app.
   let DEFAULT_ROUTE: JSX.Element = <MainApp/>;
 
   (db.get("BADASS_HOME_PAGE_SEEN") === "true") ? DEFAULT_ROUTE = <MainApp/> : DEFAULT_ROUTE = <Home/>
