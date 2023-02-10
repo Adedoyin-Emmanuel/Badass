0ff496b1fd67c45c4c8f574769c3e76ee115ead4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 02:42:31 +0100

    Updated the convert hero

--- src/components/convertHero.tsx
+++ src/components/convertHero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "./../assets/images/skull.svg";
+import Convert from "./../assets/images/convert.svg";
 
 const ConvertHero = () => {
 	return (
@@ -9,15 +9,15 @@ const ConvertHero = () => {
 					<div
 						className="col-md-6 col-sm-8 col  col-lg-4 d-flex align-items-center m-auto my-md-3 my-5"
 						id="img-container">
-						<img src={Logo} width="400" height="400" className="img-fluid m-auto"/>
+						<img src={Convert} width="400" height="400" className="img-fluid m-auto"/>
 					</div>
 
 					<div className="col-lg-8 text-center text-lg-start my-md-3">
 						<h1 className="display-5 fw-bold lh-1 mb-3 text-capitalize text-start text-md-center text-lg-start px-2 px-md-0">
-							A free tool for image manipulation
+							Convert Images with badass
 						</h1>
 						<p className="col-lg-10 fs-5 p-2 text-start text-lg-start">
-							Do all with one application, download images, convert images, remove image background.
+							Convert your images from one format to another. png, ico, jpg, jpeg etc.
 						</p>
 
 						<button className="btn btn-lg get-started brand-primary-color text-capitalize mx-md-2 my-3">
