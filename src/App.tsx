8610774db5dd74f3ae608bba6a86e93f2ca19cc9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-10 15:35:20 +0100

    Updated the MainApp page

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import "./autoload";
 import db from "./backend/db";
 function App() {
 
-  const DEFAULT_ROUTE = "";
+  let DEFAULT_ROUTE: JSX.Element =;
 
-  (db.get("BADASS_HOME_PAGE_SEEN") === "true") ? 
+  (db.get("BADASS_HOME_PAGE_SEEN") === "true") ? DEFAULT_ROUTE = "Main-Page" : DEFAULT_ROUTE = <Home/>
 
 	return (
 		<div className="App">
 			<BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={DEFAULT_ROUTE} element={<Home />} />
           <Route path="home" element={<Home/>}/>
           <Route path="support" element={<Support/>}/>
         </Routes>
--- src/pages/Main-App.tsx
+++ src/pages/Main-App.tsx
@@ -1,1 +1,12 @@
-export {}
+import React from "react";
+
+const MainApp = (): JSX.Element =>{
+    return (
+        <React.Fragment>
+                <h2>Hello world, welcome to the main app</h2>
+        </React.Fragment>
+    )
+}
+
+
+export default MainApp;
