f7ac12d957553a7a7c6db693b903d57070e13ca6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-11 15:28:09 +0100

    Finished the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import LostImage from "./../assets/images/lost.svg";
+import Button from "./../components/button";
 
 const NotFound = (): JSX.Element =>{
 	return (
 		<React.Fragment>
 
-			<section className="container-fluid ">
-
-				<section className="not-found-container">
-					<img src={LostImage} className="img-fluid m-auto"/>
+			<section className="container-fluid my-3 ">
+				<section className="d-flex align-items-center justify-content-center">
+					<h1 className="fs-1 fw-bold text-light text-capitalize my-2 p-2">Not found!</h1>
 				</section>
 
+				<section className="not-found-container d-flex align-items-center justify-content-center my-md-0 my-3">
+					<img src={LostImage} className="img-fluid m-auto lost-image p-4" width="400" height="400"/>
+				</section>
+
+				 <section className="m-auto button-container d-flex align-items-center justify-content-center my-sm-3">
+                              <Button className="brand-button-3 my-3 width-toggle-6 text-capitalize fw-bold text-dark brand-white-color" text="Go Home"></Button>
+                  </section>
 			</section>
 
 		</React.Fragment>
