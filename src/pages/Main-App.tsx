a664070c21e02de72635055a10c711d1aafd2ccd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-11 12:56:50 +0100

    Added the route checker to the main app page

--- src/pages/Main-App.tsx
+++ src/pages/Main-App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import db from "../backend/db";
+import Button from "./../components/button";
+import {navigate} from "./../includes/scripts/script";
+
 const MainApp = (): JSX.Element =>{
+
+    if(!(db.get("BADASS_HOME_PAGE_SEEN") == "true"))
+    {
+        navigate("/home");
+    }
     return (
         <React.Fragment>
                 <h2>Hello world, welcome to the main app</h2>
+
+                <Button text="hello world" className="brand-button"></Button>
         </React.Fragment>
     )
 }
