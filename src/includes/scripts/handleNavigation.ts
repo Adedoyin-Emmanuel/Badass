b7ce1fbf66d5d6abc8482c1ebbdfa2809d39b746
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-12 20:04:03 +0100

    Added the other page navigation functions

--- src/includes/scripts/handleNavigation.ts
+++ src/includes/scripts/handleNavigation.ts
@@ -1,5 +1,21 @@
 import {navigate} from  "./script";
 
+export const navigateToApp = (): void =>{
+	navigate("/app");
+}
+
+export const navigateToSettings = (): void =>{
+	navigate("/app/settings");
+}
+
+export const navigateToSupport = (): void =>{
+	navigate("/app/support");
+}
+
+export const navigateToHome = (): void =>{
+	navigate("/home");
+}
+
 export const navigateToConvert = (): void =>{
     navigate("/app/convert");
 }
