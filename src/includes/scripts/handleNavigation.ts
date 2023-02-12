e7487e79fae4c3bca18a15af7995ff91c6330eb1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-12 20:00:30 +0100

    Added the page navigation functions

---
+++ src/includes/scripts/handleNavigation.ts
@@ -0,0 +1,14 @@
+import {navigate} from  "./script";
+
+export const navigateToConvert = (): void =>{
+    navigate("/app/convert");
+}
+
+export const navigateToUpload = (): void =>{
+    navigate("/app/upload");
+}
+
+export const navigateToDownload = (): void =>{
+    navigate("/app/search");
+}
+
