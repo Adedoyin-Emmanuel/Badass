14ebcb46736bccc83f5e5078ee6a9d0c9d9f4a7c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-12 04:04:28 +0100

    Added the footer-nav file

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,4 +1,17 @@
+import React from "react";
 
+const AppFooter = (): JSX.Element =>{
+	return (
+
+		<React.Fragment>
+
+			<section className="app-footer">
+
+			</section>
+		</React.Fragment>
+
+	)
+}
 
 
 export {}
