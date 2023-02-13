4c51b63ad7eab8bc9e0b8e96b21cb940232cecf9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-13 13:07:08 +0100

    Updated restore settings function

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,9 +3,17 @@ import AppHeader from "./../components/app-header";
 import AppFooter from "./../components/app-footer";
 import * as navigate from "./../includes/scripts/handleNavigation";
 import Button from "../components/button";
-
+import db from "./../backend/db";
 
 const Settings = () => {
+
+    const restoreFactorySettings = () =>{
+        if(db.get("BADASS_HOME_PAGE_SEEN") === "true")
+        {
+          db.delete("BADASS_HOME_PAGE_SEEN");
+          navigate.navigateToHome();
+        }
+    }
 
     navigate.checkIfHomePageSeen();
     return (
@@ -21,12 +29,12 @@ const Settings = () => {
                   restore factory settings
                 </label>
                 <section className="d-flex align-items-center justify-content-center d-lg-block my-3">
-                  <Button className="brand-button-outline my-3 width-toggle text-capitalize fw-bold" text="Restore Settings" ></Button>
+                  <Button className="brand-button-outline my-3 width-toggle text-capitalize fw-bold" text="Restore Settings" onClick={restoreFactorySettings}></Button>
 
                 </section>
                 <p className="text-muted brand-small-text-2">
                   Restoring factory settings removes all the data stored on this
-                  device. You would be taken to the home screen</p>
+                  device. You would be taken to the home screen!</p>
               </section>
        <AppFooter childrenSettingsActivePage="current-active-page"/>
 
