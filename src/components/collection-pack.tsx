48a31ff8a97e67d34b67abdccc36468815de4f6e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-25 20:16:11 +0100

    Updated the collection-card button styles

--- src/components/collection-pack.tsx
+++ src/components/collection-pack.tsx
@@ -89,13 +89,13 @@ const CollectionPack = ({title, total, previewPhotoOne, previewPhotoTwo, preview
 				    </div>
 
 				    <div className="d-flex justify-content-around mt-3 my-2">
-				      <button className="p-1  px-2 rounded brand-download-image-btn text-light brand-small-text-2 shadow-sm">
+				      <button className="p-1 px-2 rounded brand-download-image-btn brand-small-text-2 shadow-sm">
 						  <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
 						    <path d="M7.5 1v9.793l-3.646-3.647a.5.5 0 0 0-.708.708l4 4a.5.5 0 0 0 .708 0l4-4a.5.5 0 0 0-.708-.708L8.5 10.793V1a.5.5 0 0 0-1 0z"/>
 						    <path d="M14.5 13.5v1a.5.5 0 0 1-.5.5h-12a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 .5.5z"/>
 						  </svg> Download
 						</button>
-				      <button type="button" className="p-1 px-2  rounded brand-small-text-2 text-capitalize text-dark brand-view-image-btn">view images</button>
+				      <button type="button" className="p-1 px-2 rounded brand-small-text-2 text-capitalize brand-view-image-btn">view images</button>
 				    </div>
 				  </div>
 				</div>
