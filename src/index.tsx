c3c79da58301ea611d98c21c8b613a093fe13d85
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-06 09:24:53 +0100

    Removed the default react logo

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -11,7 +12,25 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const config = {
+  scope: "/",
+  registerOptions: {
+    updateViaCache: "none"
+  },
+  onSuccess: (registration:any) => {
+    console.log("Service worker registration successful:", registration);
+  },
+  onUpdate: (registration:any) => {
+    console.log("Service worker update available:", registration);
+  },
+};
+
+
 //we go add the service worker later :)
+//we don add am ooo hah :)
+
+serviceWorkerRegistration.register(config);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
